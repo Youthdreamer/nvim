6e63514d1d40c3d18ed3e2c801abd14c4fde33f0
youth <95559199+Youthdreamer@users.noreply.github.com>
2026-01-19 18:06:17 +0800

    todo插件问题修复

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -8,19 +8,7 @@ return {
 	keys = {
 		{
 			"<leader>ft",
-			function()
-				local ok, _ = pcall(require, "todo-comments")
-				if not ok then
-					vim.notify(
-						"todo-comments 未加载，请检查插件是否启用",
-						vim.log.levels.WARN,
-						{ title = "Todo" }
-					)
-					return
-				end
-
-				vim.cmd("Telescope todo-comments todo theme=dropdown")
-			end,
+			"<cmd>Telescope todo-comments todo theme=dropdown<cr>",
 			desc = "TODO查询",
 		},
 	},
